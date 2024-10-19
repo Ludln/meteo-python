890732829e285fc91061e2dfa7fee2db0814e91d
Ludln <lucas.derlan@airbus.com>
2024-10-19 15:42:12 +0000

    Classe terminée

--- main.py
+++ main.py
@@ -2,19 +2,8 @@ import requests
 import json
 from datetime import datetime
 
-
-apiKey = "2257179fed7d3904654343d838db3f81"
-
-# Récupération des paramètres
-city = str(input('Enter the city you want : '))
-country = str(input('In wich county it is : '))
-
-# URL de l'API
-url = f'https://api.openweathermap.org/data/2.5/forecast?q={city},{country}&units=metric&appid={apiKey}'
-
-# Certificat
-REQUESTED_CA_BUNDLE = 'airbus-ca.crt'
-
+from  utilities import send_request
+s
 # Envoie de la requête
 response = requests.get(f"{url}", verify=REQUESTED_CA_BUNDLE)
 if response.status_code == 404:                     # Si le status code de la requête est 404,
